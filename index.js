a4e6dc558a70fa803c2623afbf5a55ee19cee702
pavan987 <gondhi@usc.edu>
2017-05-07 03:58:44 -0700

    make it better

--- index.js
+++ index.js
@@ -22,7 +22,14 @@ var port = 8082;
     app.use(bodyParser.json())
     app.use(cookieParser());
     app.use(session({ secret: 'keyboard cat' }));
+     app.use(function(req, res, next) { //allow cross origin requests
+        res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        next();
+    });
     app.use(passport.initialize());
+
     app.use('/api', router);
     app.use(cors());
 }
